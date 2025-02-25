4ac9af1b191672ff4ad24100bfdf545b7f75ed4f
Dipanjan <dipanjanr640@gmail.com>
2025-02-25 21:16:12 +0530

    module update

--- infrastructure/modules/variables.tf
+++ infrastructure/modules/variables.tf
@@ -1,1 +1,40 @@
+variable "rg_name" {
+  default = "terraformrg"
+  type = string
+  
+}
+variable "location" {
+  type = string
+  
+}
+variable "vnet_name" {
+    type = string
+  
+}
+variable "nic_name" {
+    type = string
+  
+}
+variable "vm_name" {
+    type = string
+  
+}
+variable "vm_size" {
+  type        = string
+  default     = "Standard_DS1_v2"
+  
+}
 
+variable "admin_username" {
+  type        = string
+  
+}
+
+variable "admin_password" {
+  type        = string
+  
+}
+variable "storage_ac_type" {
+    type = string
+  
+}
